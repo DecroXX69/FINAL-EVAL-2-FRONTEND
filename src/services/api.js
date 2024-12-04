ccd5cc43e2897fd95b6e45aba751cc35f96eefe2
DecroXX <73951267+DecroXX69@users.noreply.github.com>
2024-12-04 09:06:16 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const API = axios.create({
-  baseURL: 'https://final-eval-2-backend.vercel.app/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
   headers: {
     'Content-Type': 'application/json',
   }
